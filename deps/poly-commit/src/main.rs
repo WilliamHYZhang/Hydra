4d562ca933761f40c8cf7073285263334536d350
William Zhang <WilliamHYZhang@gmail.com>
2021-02-15 21:18:41 -0500

    init

---
+++ deps/poly-commit/src/main.rs
@@ -0,0 +1,56 @@
+use std::env;
+
+use ark_poly::{MultilinearExtension, SparseMultilinearExtension};
+use ark_bls12_381::Bls12_381;
+use ark_ec::PairingEngine;
+use ark_ff::biginteger::BigInteger256;
+use ark_std::test_rng;
+use ark_poly_commit::multilinear_pc::MultilinearPC;
+
+type E = Bls12_381;
+type Fr = <E as PairingEngine>::Fr;
+
+fn convert_bigint(num: u64) -> Fr {
+  return Fr::new(BigInteger256([ 064, 0u64, 0u64, num ]));
+}
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+  println!("{:?}", args);
+  let num_gates = args[1].parse::<usize>().unwrap();
+
+  let mut poly_vec = Vec::new();
+  for i in 0..num_gates {
+    poly_vec.push((i, convert_bigint(args[i+1].parse::<u64>().unwrap())));
+  }
+
+  let num_points = args[num_gates+2].parse::<usize>().unwrap();
+  let point_dim = args[num_gates+3].parse::<usize>().unwrap();
+  let offset = num_gates+4;
+
+  let mut vec_points = Vec::new();
+  for i in 0..num_points {
+    vec_points.push(vec![]);
+    for j in 0..point_dim {
+      let index = offset + i * point_dim + j;
+      println!("{}", index);
+      vec_points[i].push(convert_bigint(args[index].parse::<u64>().unwrap()));
+    }
+  }
+
+  let poly = SparseMultilinearExtension::from_evaluations(2, &poly_vec);
+
+  let mut rng = test_rng();
+  let uni_params = MultilinearPC::<E>::setup(2, &mut rng);
+
+  let nv = poly.num_vars;
+  let (ck, vk) = MultilinearPC::<E>::trim(&uni_params, nv);
+
+  let com = MultilinearPC::commit(&ck, &poly);
+
+  for vec_point in vec_points {
+    let proof = MultilinearPC::open(&ck, &poly, &vec_point);
+    let value = poly.evaluate(&vec_point).unwrap();
+    let _result = MultilinearPC::check(&vk, &com, &vec_point, value, &proof);
+  }
+}
---
+++ protocols/main/subcircuit.cpp
@@ -0,0 +1,565 @@
+/*
+William Zhang
+*INSERT DATE HERE*
+*/
+
+#include <bits/stdc++.h>
+
+#define MASK 4294967295 //2^32-1
+#define PRIME 2305843009213693951 //2^61-1
+
+//fast log2 (g++)
+#define FAST_LOG2(x) (sizeof(unsigned long)*8 - 1 - __builtin_clzl((unsigned long)(x)))
+#define FAST_LOG2_UP(x) ({ unsigned long log = FAST_LOG2(x); ((x) - (1 << log)) ? log + 1 : log; })
+
+using namespace std;
+
+typedef struct gate{
+  int type; // 0 add, 1 mult
+  uint64_t val;
+  int in1;
+  int in2;
+  uint64_t wire;
+  uint64_t beta;
+} gate;
+
+typedef vector<vector<gate>> circ;
+circ c;
+vector<circ> subcirc;
+
+uint64_t mod(uint64_t x){
+  uint64_t ret = (x >> 61) + (x & PRIME);
+  return ret >= PRIME ? ret - PRIME : ret;
+}
+
+inline uint64_t modMult(uint64_t x, uint64_t y){
+  uint64_t hi_x = x >> 32;
+  uint64_t hi_y = y >> 32;
+  uint64_t low_x = x & MASK;
+  uint64_t low_y = y & MASK;
+  uint64_t piece1 = mod((hi_x * hi_y) << 3);
+  uint64_t z = (hi_x * low_y + hi_y * low_x);
+  uint64_t hi_z = z >> 32;
+  uint64_t low_z = z & MASK;
+  uint64_t piece2 = mod((hi_z << 3) + mod((low_z << 32)));
+  uint64_t piece3 = mod(low_x * low_y);
+  uint64_t ret = mod(piece1 + piece2 + piece3);
+  return ret;
+}
+
+uint64_t modPow(uint64_t b, uint64_t e){
+  uint64_t ret;
+  if(e == 1){
+    return b;
+  }
+  if(e == 0){
+    return 1;
+  }
+  if((e & 1) == 0){
+    ret = modPow(b, (e >> 1));
+    return modMult(ret, ret);
+  }
+  else{
+    return modMult(modPow(b, e-1), b);
+  }
+}
+
+void eucl(uint64_t u, uint64_t &u1, uint64_t &u2, uint64_t &u3){
+  u1 = 1;
+  u2 = 0;
+  u3 = u;
+  uint64_t v1 = 0;
+  uint64_t v2 = 1;
+  uint64_t v3 = PRIME;
+  uint64_t q;
+  uint64_t t1;
+  uint64_t t2;
+  uint64_t t3;
+  do{
+    q = u3 / v3;
+    t1 = mod(u1+PRIME - modMult(q, v1));
+    t2 = mod(u2+PRIME - modMult(q, v2));
+    t3 = mod(u3+PRIME - modMult(q, v3));
+    u1 = v1;
+    u2 = v2;
+    u3 = v3;
+    v1 = t1;
+    v2 = t2;
+    v3 = t3;
+  } while(v3 != 0 && v3 != PRIME);
+}
+
+uint64_t inv(uint64_t a)
+{
+  uint64_t u1;
+  uint64_t u2;
+  uint64_t u3;
+  eucl(a, u1, u2, u3);
+  if(u3 == 1){
+    return mod(u1);
+  }
+  else{
+    return 0;
+  }
+}
+
+uint64_t modDiv(uint64_t a, uint64_t b){
+  uint64_t i = inv(b);
+  return modMult(i, a);
+}
+
+uint64_t interp(uint64_t v, vector<uint64_t> &r, uint64_t n, int r_i){
+  //cout << "interp called with v: " << v << " n: " << n << " ri: " << r_i << endl;
+  uint64_t x = v;
+  uint64_t c = 1;
+  for(uint64_t i = 0; i < n; ++i){
+    //cout << "rbit: " << r[r_i + i];
+    if(x & 1){
+      //cout << " yes ";
+      c = modMult(c, r[r_i + i]);
+    }
+    else{
+      //cout << " no ";
+      //c = modMult(c, 1);
+      c = modMult(c, 1+PRIME - r[r_i + i]);
+    }
+    //cout << endl;
+    x >>= 1;
+  }
+  //cout << "interp: " << c << endl;
+  return c;
+}
+
+uint64_t beta(int mi, vector<uint64_t> &z, uint64_t k){
+  //cout << "beta called with mi: " << mi << " k: " << k << " z: " << endl;
+  //for(auto i : z) cout << i << " "; cout << endl; 
+  uint64_t ret = 1;
+  uint64_t x = k;
+  for(int i = 0; i < mi; ++i){
+    if(x & 1){
+      ret = modMult(ret, z[i]);
+    }
+    else{
+      ret = modMult(ret, 1+PRIME - z[i]);
+    }
+    x >>= 1;
+  }
+  //cout << "returning: " << ret << endl;
+  return ret;
+}
+
+uint64_t w_i(int mi, int ni, int i, vector<uint64_t> &r, int r_i, int b){
+  uint64_t ret = 0;
+  for(uint64_t k = 0; k < ni; ++k){
+    //cout << "gate val: " << subcirc[b][i][k].val  << endl; //<< " interp: " << interp(k, r, mi, r_i) << endl;
+    ret = mod(ret + modMult(subcirc[b][i][k].val, interp(k, r, mi, r_i)));
+    //cout << "ret: " << ret << endl;
+  }
+  return ret;
+}
+
+uint64_t extrap(vector<uint64_t> &v, uint64_t n, uint64_t r){
+  uint64_t ret = 0;
+  uint64_t mult = 1;
+  for(uint64_t i = 0; i < n; ++i){
+    mult = 1;
+    for(uint64_t j = 0; j < n; ++j){
+      if(i > j){
+        mult = (modMult(modMult(mult, mod(r - j+PRIME)), inv(i - j)));
+      }
+      if(i < j){
+        mult = modMult(modMult(mult, mod(r - j+PRIME)), inv(mod(i+PRIME - j)));
+      }
+    }
+    ret = mod(ret + modMult(mult, v[i]));
+  }
+  return ret;
+}
+
+uint64_t log2_max, gates_max;
+
+uint64_t sumcheck(int lvl, vector<uint64_t> &z, uint64_t ri, int b, vector<pair<uint64_t, uint64_t>> &points){
+  int mi = FAST_LOG2_UP(subcirc[b][lvl].size());
+  int ni = subcirc[b][lvl].size();
+  int mip1 = FAST_LOG2_UP(subcirc[b][lvl-1].size());
+  int nip1 = subcirc[b][lvl-1].size();
+
+  //cout << "lvl: " << lvl << " ri: " << ri << " b: " << b << endl;
+  int num_var = mi + 2 * mip1;
+  //cout << "NUM VAR: " << num_var << endl;
+  int num_effective = modPow(2, mip1);
+  uint64_t alpha_p, beta_p;
+
+  vector<uint64_t> r;
+  for(int i = 0; i < num_var; ++i){
+    r.push_back(rand());
+  }
+  
+  //cout << "r set up" << endl;
+
+  for(uint64_t k = 0; k < ni; ++k){
+    subcirc[b][lvl][k].beta = beta(mi, z, k);
+    //cout << "setting lvl: " << lvl << " id: " << k << " beta to: " << subcirc[b][lvl][k].beta << endl;
+    subcirc[b][lvl][k].wire = 1;
+  }
+
+  //cout << "beta set up" << endl;
+
+  vector<vector<uint64_t>> poly;
+  for(int k = 0; k < num_var; ++k){
+    poly.push_back({0, 0, 0});
+  }
+  //cout << "poly set up" << endl;
+
+  vector<uint64_t> val;
+  for(int k = 0; k < num_effective; ++k){
+    val.push_back(k < nip1 ? subcirc[b][lvl-1][k].val : 0);
+  }
+
+  //cout << "val set up" << endl;
+
+  uint64_t beta = 0;
+  uint64_t k_j;
+  uint64_t wire = 0;
+  uint64_t w1 = 0, w2 = 0;
+  uint64_t index;
+  
+  for(int j = 0; j < num_var; ++j){
+    //cout << "round: " << j << endl;
+    if(j > mi && (j != mi + mip1)){
+      for(int k = 0; k < num_effective; ++k){
+        index = k >> 1;
+        if(k & 1){
+          val[index] = mod(val[index] + modMult(val[k], r[j-1]));
+        }
+        else{
+          val[index] = modMult(val[k], 1+PRIME - r[j-1]);
+        }
+      }
+      num_effective >>= 1;
+    }
+    if(j == mi + mip1){
+      //cout << "RESETTING" << endl;
+      num_effective = modPow(2, mip1);
+      for(int k = 0; k < num_effective; ++k){
+        val[k] = k < nip1 ? subcirc[b][lvl-1][k].val : 0;
+      }
+      w1 = w_i(mip1, nip1, lvl-1, r, mi, b);
+      //cout << "w1: " << w1 << endl;
+    }
+    if(j == mi){
+      beta = subcirc[b][lvl][0].beta;
+    }
+    for(int k = 0; k < ni; ++k){
+      if(j < mi){
+        w1 = subcirc[b][lvl-1][subcirc[b][lvl][k].in1].val;
+        w2 = subcirc[b][lvl-1][subcirc[b][lvl][k].in2].val;
+      }
+      else if(j < mi + mip1){
+        w2 = subcirc[b][lvl-1][subcirc[b][lvl][k].in2].val;
+      }
+      if(j < mi){
+        k_j = k >> j;
+        if(k_j & 1){
+          subcirc[b][lvl][k].beta = modMult(subcirc[b][lvl][k].beta, inv(z[j]));
+        }
+        else{
+          subcirc[b][lvl][k].beta = modMult(subcirc[b][lvl][k].beta, inv(1+PRIME - z[j]));
+        }
+      }
+
+      for(int m = 0; m <= 2; ++m){
+        if(j < mi){
+          if(m == 0){
+            beta = modMult(subcirc[b][lvl][k].beta, modMult(1+PRIME - z[j], 1+PRIME - m));
+          }
+          else if(m == 1){
+            beta = modMult(subcirc[b][lvl][k].beta, modMult(z[j], m));
+          }
+          else{
+            beta = modMult(subcirc[b][lvl][k].beta, mod(modMult(1+PRIME - z[j], 1+PRIME - m) + modMult(z[j], m)));
+          }
+          if(m == 2){
+            subcirc[b][lvl][k].beta = modMult(subcirc[b][lvl][k].beta, mod(modMult(1+PRIME - z[j], 1+PRIME - r[j]) + modMult(z[j], r[j])));
+          }
+          if(k_j & 1){
+            if(m == 0){
+              continue;
+            }
+            else{
+              wire = modMult(subcirc[b][lvl][k].wire, m);
+            }
+            if(m == 2){
+              subcirc[b][lvl][k].wire = modMult(subcirc[b][lvl][k].wire, r[j]);
+            }
+          }
+          else{
+            if(m==1){
+              continue;
+            }
+            else{
+              wire = modMult(subcirc[b][lvl][k].wire, 1+PRIME - m);
+            }
+            if(m == 2){
+              subcirc[b][lvl][k].wire = modMult(subcirc[b][lvl][k].wire, 1+PRIME - r[j]);
+            }
+          }
+        }
+        if(j >= mi && j < mi+mip1){
+          index = subcirc[b][lvl][k].in1 >> (j - mi);
+          if(index & 1){
+            if(m == 0){
+              continue;
+            }
+            else if(m == 1){
+              w1 = val[index];
+            }
+            else{
+              w1 = mod(modMult(val[index], m) + modMult(val[index-1], 1+PRIME - m));
+            }
+            wire = modMult(subcirc[b][lvl][k].wire, m);
+            if(m == 2){
+              subcirc[b][lvl][k].wire = modMult(subcirc[b][lvl][k].wire, r[j]);
+            }
+          }
+          else{
+            if(m == 1){
+              continue;
+            }
+            else if(m == 0){
+              w1 = val[index];
+            }
+            else{
+              w1 = mod(modMult(val[index+1], m) + modMult(val[index], 1+PRIME - m));
+            }
+            wire = modMult(subcirc[b][lvl][k].wire, 1+PRIME - m);
+            if(m == 2){
+              subcirc[b][lvl][k].wire = modMult(subcirc[b][lvl][k].wire, 1+PRIME - r[j]);
+            }
+          }
+        }
+        else if(j >= mi+mip1){
+          index = subcirc[b][lvl][k].in2 >> (j - mi - mip1);
+          if(index & 1){
+            if(m == 0){
+              continue;
+            }
+            else if(m == 1){
+              w2 = val[index];
+            }
+            else{
+              w2 = mod(modMult(val[index], m) + modMult(val[index-1], 1+PRIME - m));
+            }
+            wire = modMult(subcirc[b][lvl][k].wire, m);
+            if(m == 2){
+              subcirc[b][lvl][k].wire = modMult(subcirc[b][lvl][k].wire, r[j]);
+            }
+          }
+          else{
+            if(m == 1){
+              continue;
+            }
+            else if(m == 0){
+              w2 = val[index];
+            }
+            else{
+              w2 = mod(modMult(val[index+1], m) + modMult(val[index], 1+PRIME - m));
+            }
+            wire = modMult(subcirc[b][lvl][k].wire, 1+PRIME - m);
+            if(m == 2){
+              subcirc[b][lvl][k].wire = modMult(subcirc[b][lvl][k].wire, 1+PRIME - r[j]);
+            }
+          }
+        }
+
+        //cout << "beta: " << beta << " wire: " << wire << endl;
+        if(subcirc[b][lvl][k].type == 0){
+          poly[j][m] = mod(poly[j][m] + mod(modMult(beta, modMult(wire, mod(w1 + w2)))));
+        }
+        else if(subcirc[b][lvl][k].type == 1){
+          poly[j][m] = mod(poly[j][m] + mod(modMult(beta, modMult(wire, modMult(w1, w2)))));
+        }
+       
+        //cout << "j: " << j << " m: " << m << " poly: " << poly[j][m] << endl;
+      }
+    }
+  }
+  if((lvl != subcirc[b].size()-1) && (mod(poly[0][0]+poly[0][1]) != ri) && (mod(poly[0][0]+poly[0][1]) != ri-PRIME) && (mod(poly[0][0]+poly[0][1]) != ri+PRIME)){
+    cout << "PROTOCOL FAILED: claimed value is incorrect" << endl;
+  }
+
+  uint64_t check = 0;
+  for(int j = 1; j < num_var; j++) {
+    check = extrap(poly[j-1], 3, r[j-1]);
+    if((check != mod(poly[j][0]+poly[j][1])) && (check != (mod(poly[j][0]+poly[j][1])+PRIME)) && (check != mod(poly[j][0]+poly[j][1])-PRIME)){
+      cout << "PROTOCOL FAILED: intermediate sumcheck message is incorrect" << " b " << b << endl;
+    }
+  }
+  w2 = w_i(mip1, nip1, lvl-1, r, mi+mip1, b);
+
+  uint64_t test_wire_add = mod(w1 + w2);
+  uint64_t test_wire_mult = mod(modMult(w1, w2));
+  uint64_t fz = 0;
+  for(int k = 0; k < ni; ++k){
+    if(subcirc[b][lvl][k].type == 0){
+      fz = mod(fz + modMult(beta, modMult(subcirc[b][lvl][k].wire, test_wire_add)));
+    }
+    else if(subcirc[b][lvl][k].type == 1){
+      fz = mod(fz + modMult(beta, modMult(subcirc[b][lvl][k].wire, test_wire_mult)));
+    }
+  }
+  check = extrap(poly[num_var-1], 3, r[num_var-1]);
+  if((check != fz) && (check != fz + PRIME) && (check != fz-PRIME)){
+    cout << "PROTOCOL FAILED: final check is incorrect" << endl;
+  }
+
+  vector<uint64_t> lpoly;
+  vector<uint64_t> point(mip1);
+  vector<uint64_t> vec(2);
+
+  for(int k = 0; k < mip1+1; k++){
+    for(int j = 0; j < mip1; j++){
+      vec[0]=r[mi+j];
+      vec[1]=r[mi+mip1+j];
+      point[j] = extrap(vec, 2, k);
+    }
+    lpoly.push_back(w_i(mip1, nip1, lvl-1, point, 0, b)); 
+  }
+
+  if((w1 != lpoly[0]) && (w1 != lpoly[0] + PRIME) && (w1 != lpoly[0]-PRIME)){
+    cout << "PROTOCOL FAILED: new claim is incorrect" << endl;
+  }
+  if((w2 != lpoly[1]) && (w2 != lpoly[1] + PRIME) && (w2 != lpoly[1]-PRIME)){
+    cout << "PROTOCOL FAILED: new claim is incorrect" << endl;
+  }
+
+  uint64_t t = mod(0);
+  for(int j = 0; j < mip1; j++)
+  {
+    vec[0]=r[mi+j];
+    vec[1]=r[mi+mip1+j];
+    z[j] = extrap(vec, 2, t);
+  }
+
+  uint64_t ret = extrap(lpoly, mip1+1, t);
+  return ret;
+}
+
+const int DEPTH = 10000, WIDTH = 128;
+void create_circuit(){
+  for(int i = 0; i < DEPTH; ++i){
+    vector<gate> level;
+    for(int j = 0; j < WIDTH; ++j){
+      gate g;
+      if(i == 0){
+        g.val = rand();
+      }
+      else{
+        g.type = rand()%2;
+        g.in1 = rand()%WIDTH;
+        g.in2 = rand()%WIDTH;
+      }
+      level.push_back(g);
+    }
+    c.push_back(level);
+  }
+}
+
+void eval_circuit(){
+  for(int i = 1; i < c.size(); ++i){
+    log2_max = max(log2_max, FAST_LOG2_UP(c[i].size()));
+    for(int j = 0; j < c[i].size(); ++j){
+      if(c[i][j].type == 0){
+        c[i][j].val = mod(c[i-1][c[i][j].in1].val + c[i-1][c[i][j].in2].val);
+      }
+      else if(c[i][j].type == 1){
+        c[i][j].val = modMult(c[i-1][c[i][j].in1].val, c[i-1][c[i][j].in2].val);
+      }
+    }
+  }
+}
+
+const int SUB_WIDTH = 100;
+void pipeline(){
+  for(int i = 0; i < c.size(); i += SUB_WIDTH){
+    vector<vector<gate>> sub(c.begin()+(i==0 ? 0 : i-1), i+SUB_WIDTH >= c.size() ? c.end() : c.begin()+i+SUB_WIDTH);
+    subcirc.push_back(sub);
+  }
+}
+
+const bool PIPELINE = false;
+
+int main(){
+  cout << "creating circuit..." << endl;
+  auto start = chrono::steady_clock::now();
+  create_circuit();
+  auto end = chrono::steady_clock::now();
+  cout << "creation time: " << chrono::duration <double, milli> (end-start).count()/1000 << " s\n" << endl;
+
+  cout << "evaluating circuit..." << endl;
+  start = chrono::steady_clock::now();
+  eval_circuit();
+  end = chrono::steady_clock::now();
+  cout << "done, output: " << c.back()[0].val << endl;
+  cout << "evaluation time: " << chrono::duration <double, milli> (end-start).count()/1000 << " s\n" << endl;
+
+  cout << "pipelining circuit..." << endl;
+  start = chrono::steady_clock::now();
+  pipeline();
+  end = chrono::steady_clock::now();
+  cout << "pipeline time: " << chrono::duration <double, milli> (end-start).count()/1000 << " s\n" << endl;
+
+
+  cout << "proving/verifying..." << endl;
+  start = chrono::steady_clock::now();
+
+  if(!PIPELINE){
+    uint64_t ri;
+    vector<uint64_t> z; 
+    for(int j = 0, num = FAST_LOG2_UP(c[c.size()-1].size()); j < num; ++j){
+      z.push_back(rand());
+    }
+    for(int b = 0; b < subcirc.size(); ++b){
+      vector<pair<uint64_t, uint64_t>> points;
+      for(int i = subcirc[b].size()-1; i > 0; --i){
+        //cout << "b: " << b << " i: " << i << endl;
+        ri = sumcheck(i, z, ri, b, points);
+      }
+    }
+  }
+  else {
+    #pragma omp parallel for
+    for(int b = 0; b < subcirc.size(); ++b){
+      uint64_t ri = 0;
+      vector<uint64_t> z;
+      int num = FAST_LOG2_UP(c[c.size()-1].size());
+      for(int j = 0; j < num; ++j){
+        z.push_back(rand());
+      }
+      vector<pair<uint64_t, uint64_t>> points;
+      for(int i = subcirc[b].size()-1; i > 0; --i){
+        //cout << "b: " << b << " i: " << i << endl;
+        ri = sumcheck(i, z, ri, b, points);
+      }
+
+      string cmd = "cd ../../deps/poly-commit cargo run ";
+      cmd += to_string(subcirc[b][0].size()) + " ";
+      for(int i = 0; i < subcirc[b][0].size(); ++i){
+        cmd += to_string(subcirc[b][0][i].val) + " ";
+      }
+      cmd += to_string(num) + " ";
+      cmd += "1 ";
+      for(int i = 0; i < num; ++i) {
+        cmd += to_string(z[i]) + " ";
+      }
+      int err = system(cmd.c_str());
+      if(err){
+        cout << "PROTOCOL FAILED: polynomial commitment error" << endl;
+      }
+    }
+  }
+  
+  end = chrono::steady_clock::now();
+  cout << "done." << endl;
+  cout << "protocol time: " << chrono::duration <double, milli> (end-start).count()/1000 << " s\n" << endl;
+}
